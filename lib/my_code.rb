8d6c02c3cfcefe29e2a292d109a77b459730d52d
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-31 14:39:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,79 @@
-# My Code here....
+def map_to_negativize(source_array)
+  i = 0
+  result = []
+  
+  while i < source_array.length do
+    result[i] = source_array[i] * (-1)
+    i += 1
+  end
+  result
+end
+
+def map_to_no_change(source_array)
+  i = 0
+  result = []
+  
+  while i < source_array.length do
+    result[i] = source_array[i] 
+    i += 1
+  end
+  result
+end 
+
+def map_to_double(source_array)
+  i = 0
+  result = []
+  
+  while i < source_array.length do
+    result[i] = source_array[i] * 2 
+    i += 1
+  end
+  result
+end 
+
+def map_to_square(source_array)
+  i = 0
+  result = []
+  
+  while i < source_array.length do
+    result[i] = source_array[i] ** 2 
+    i += 1
+  end
+  result
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  result = starting_point
+  i = 0
+  
+  while i < source_array.length do
+    result += source_array[i]
+    i += 1
+  end
+  return result
+end 
+
+def reduce_to_all_true(source_array)
+  i = 0
+  
+  while i < source_array.length do
+    if source_array[i] == false
+      return false
+    else i += 1
+    end
+  end
+  return true
+end 
+
+def reduce_to_any_true(source_array)
+  i = 0
+  
+  while i < source_array.length do
+    if source_array[i] == true
+      return true
+    end
+    i += 1
+  end
+  return false
+end 
+
