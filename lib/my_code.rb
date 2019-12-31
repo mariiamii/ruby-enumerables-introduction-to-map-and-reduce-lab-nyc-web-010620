8e678c4b2ea8f64a22970f6fe25ee091d825398b
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-31 14:18:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,7 @@ def map_to_negativize(source_array)
   result = []
   
   while i < source_array.length do
-    result[i] = source_array[i]*(-1)
+    result[i] = source_array[i] * (-1)
     i += 1
   end
   result
@@ -16,7 +16,7 @@ def map_to_no_change(source_array)
   result = []
   
   while i < source_array.length do
-    result[i] = source_array[i]
+    result[i] = source_array[i] * 2 
     i += 1
   end
   result
