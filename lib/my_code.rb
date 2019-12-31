b3876a3516a3a1e4626efac73775b1c3a035566d
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-31 14:21:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,6 +42,15 @@ def map_to_square(source_array)
   result
 end
 
-reduce_to_total(source_array, starting_point)
-reduce_to_all_true(source_array)
-reduce_to_any_true(source_array)
+def reduce_to_total(source_array, starting_point)
+  
+end 
+
+def reduce_to_all_true(source_array)
+  
+end 
+
+def reduce_to_any_true(source_array)
+  
+end 
+
